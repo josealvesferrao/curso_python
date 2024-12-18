40febc606f92b7ad484d7632e4150522783e9d17
José Ferrão <jose.ferrao@insa.min-saude.pt>
2024-12-18 17:14:56 +0000

    sessao_4_update

--- ex4_doencas.py
+++ ex4_doencas.py
@@ -27,15 +27,25 @@ def contagens (ficheiro):
     casos_febre_q = 0
 
     casos_gonorreia = 0
+
+    index = -1 # Iniciliazação da variável "index", que vai permitir identificar o índice de cada elemento da lista "headers" correspondente ao cabeçalho da tabela. Iniciamos este contador em "-1" para corresponder ao índice "zero" na primeira iteração do ciclo abaixo
+    
+    for i in headers: # ciclo para iterar nos elementos da lista "headers" correspondente ao cabeçalho da tabela, para obter o índice de cada elemento
+        index = index + 1
+        print("Coluna: ", i, "----> Índice: ", index) # Aqui estamos a imprimir a designação de todas as colunas e do índice correspondente
+        if i == "doenca_de_declaracao_obrigatoria": # Aqui conseguimos obter e guardar numa variável um índice específico. Se o elemento da lista for igual à string pretendida, guardar o índice numa variável que vai ser usada posteriormente. Ver exemplo da "Febre Q + Algarve", em que é usada esta variável em vez do índice "3"
+            indice_coluna_nome_doenca = index
+            print("O índice da coluna com o nome da doença é: ", index)
     
     for linha in tabela: # iterar com um ciclo for, cada linha da tabela que foi convertida numa lista com 7 elementos, correspondentes a cada uma das 7 colunas da tabela
         #print(linha)
         total_casos = total_casos + int(linha[6]) # acumulador (acumula/soma os valores dos casos notificados)
         if linha[3] not in lista_doencas: # se a doença ainda não existir dentro da lista de doenças, então:
             lista_doencas.append(linha[3]) # adicionar a nova doença à lista de doenças
-        if linha[3] == "Febre Q" and linha[2] == "Algarve": # Se a doença é Febre Q e a residência é Algarve, então:
+        if linha[indice_coluna_nome_doenca] == "Febre Q" and linha[2] == "Algarve": # Se a doença é Febre Q e a residência é Algarve, então:
             casos_febre_q = casos_febre_q + int(linha[6]) # acumulador (acumula/soma os valores dos casos notificados)
-        if linha[3] == "Gonorreia" and linha[4] == "Masculino" and linha[5] == "15-24 anos" and linha[2] == "Area Metropolitana de Lisboa":
+        #if linha[3] == "Gonorreia" and linha[4] == "Masculino" and linha[5] == "15-24 anos" and linha[2] == "Area Metropolitana de Lisboa":
+        if "Gonorreia" in linha[3] and linha[4] == "Masculino" and linha[5] == "15-24 anos" and linha[2] == "Area Metropolitana de Lisboa": # Aqui, para o match da string "Gonorreia", em vez de usar: 'if linha[3] == "Gonorreia"' que só iria identificar notificações exatamente com esta designação e não encontraria as notificações do tipo "Infecaµes Gonococicas (Gonorreia)", optei por usar: 'if "Gonorreia" in linha[3]' 
             casos_gonorreia = casos_gonorreia + int(linha[6])
 
     print("O nº total de doenças notificadas foi de: ", len(lista_doencas), ", e o nº total de casos notificados foi de: ", total_casos, "\n")
@@ -45,9 +55,9 @@ def contagens (ficheiro):
     print("O nº de casos de Febre Q notificados no Algarve é: ", casos_febre_q, "\n")
 
     print("O nº de casos de Gonorreia em indivíduos do sexo masculino entre os 15 e os 24 anos de idade residentes na AML é de: ", casos_gonorreia, "\n")
-    
+    print("cabecalho: ", headers)
     data.close()
 
-contagens("C:\\Users\\jose.ferrao\\Desktop\\Curso_Python_Out2024\\Slides_novos_2024\\Sessao_4_5_6\\doencas-de-declaracao-obrigatoria.csv")
+contagens("C:\\Users\\jose.ferrao\\Desktop\\Curso_Python_Out2024\\Slides_novos_2024\\Sessao_4\\doencas-de-declaracao-obrigatoria.csv") #Substituir pelo caminho completo para o ficheiro no vosso computador
         
         
